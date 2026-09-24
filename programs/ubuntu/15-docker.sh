d5a8322f2362c46da8118f1a96b4563b26cedbcb
Maicon Cerutti <dev.cerutti.maicon@gmail.com>
2026-09-24 13:11:55 -0400

    featÇ ajustado instalacao docker

--- programs/ubuntu/15-docker.sh
+++ programs/ubuntu/15-docker.sh
@@ -2,15 +2,26 @@
 
 ## Instalação de dependências
 
-sudo apt install apt-transport-https curl gnupg-agent ca-certificates software-properties-common -y &&
+# Add Docker's official GPG key:
+sudo apt update
+sudo apt install ca-certificates curl -y
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
 
-## Instalação Docker no Ubuntu
+# Add the repository to Apt sources:
+sudo tee /etc/apt/sources.list.d/docker.sources <<EOF
+Types: deb
+URIs: https://download.docker.com/linux/ubuntu
+Suites: $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}")
+Components: stable
+Architectures: $(dpkg --print-architecture)
+Signed-By: /etc/apt/keyrings/docker.asc
+EOF
 
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add - &&
+sudo apt update &&
 
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable" &&
-
-sudo apt install docker-ce docker-ce-cli containerd.io -y &&
+sudo apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y &&
 
 sudo usermod -aG docker $USER && 
 
